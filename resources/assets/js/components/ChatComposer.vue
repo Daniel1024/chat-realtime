9968a195484aa86208b641b4f7398ad1a258c2a5
Daniel López <tierra_santa_9@hotmail.com>
2017-02-25 23:57:16 -0500

    Tratando de conectarme al pusher

---
+++ resources/assets/js/components/ChatComposer.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+<div class="chat-composer">
+    <input
+        class="form-control"
+        v-model="messageText"
+        @keyup.enter="sendMessage"
+        type="text"
+        placeholder="Start typing your message...">
+    <button class="btn btn-primary"
+        @click.prevent="sendMessage">Send</button>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            messageText: ''
+        }
+    },
+    methods: {
+        sendMessage() {
+            if (this.messageText != '') {
+                this.$emit('messagesent', this.messageText);
+                this.messageText = '';
+            }
+        }
+    }
+}
+</script>
+<style lang="scss">
+    .chat-composer {
+        display: flex;
+        input {
+            flex: 1 auto;
+        }
+        button {
+            border-radius: 0;
+        }
+    }
+    
+</style>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,40 @@
 |
 */
 
+use App\Events\MessagePosted;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('chat', function () {
+        return view('chat');
+    })->name('chat');
+
+    Route::get('messages', function () {
+        return \App\Message::with('user')->newQuery()->get();
+    });
+
+    Route::post('message', function (\Illuminate\Http\Request $request) {
+
+        $message = \App\Message::query()
+            ->create([
+                'message'   => $request->get('message'),
+                'user_id'   => auth()->id(),
+            ]);
+
+        // Announce that a new message has been posted
+        broadcast(new MessagePosted($message, auth()->user()))->toOthers();
+
+        return [
+            'message'   => $message->message,
+            'user'      => ['name' => auth()->user()->name],
+        ];
+    });
 });
